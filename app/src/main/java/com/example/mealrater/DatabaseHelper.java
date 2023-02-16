df243393a16ab0a4813ba4984a84e3844d4d230a
AhmedMurshid <ahmed.abdullahione@gmail.com>
2023-02-16 18:01:21 -0500

    added the table for the dish rater

--- app/src/main/java/com/example/mealrater/DatabaseHelper.java
+++ app/src/main/java/com/example/mealrater/DatabaseHelper.java
@@ -1,41 +1,79 @@
 package com.example.mealrater;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.database.Cursor;
 
-import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "MYRATES.db";
-    private static  final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "MYRATES.db";
+    public static final int DATABASE_VERSION = 1;
+    // started here
+    public static final String TABLE_NAME = "rating_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "RESTAURANT";
+    public static final String COL_4 = "DISH";
+    public static final String COL_5 = "MARKS";
 
     private static final String CREATE_TABLE =
             // this is an example
             "create table rating (_id interger primary key autoincrement,"
-            + "city text, state text, zipcode text,"
-            + "phonenumber text, cellnumber text,"
-            + "email text, birthday text);";
+                    + "NAME TEXT, RESTAURANT TEXT, DISH TEXT ,MARKS INTEGER);";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CREATE_TABLE);
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.w(DatabaseHelper.class.getName(),
-                "Upgrading database from version " + oldVersion +" to "
+                "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS rating");
-        onCreate(sqLiteDatabase);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name, String restraurant, String dish, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, restraurant);
+        contentValues.put(COL_4, dish);
+        contentValues.put(COL_5, marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("select * from "+TABLE_NAME,null);
+    }
+
+    public boolean updateData(String id,String name,String restaurant,String dish,String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,restaurant);
+        contentValues.put(COL_4,dish);
+        contentValues.put(COL_5,marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
     }
 }
